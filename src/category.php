f102ec1eada503451c3167083b55eef31c6116fd
btxdev <limecompro@gmail.com>
2023-02-21 05:43:18 +0500

    fix: clean url

--- src/category.php
+++ src/category.php
@@ -1,12 +1,14 @@
 <?php
 
-  if(!isset($_GET['q'])) {
+  if(!isset($_GET['q']) || !preg_match('/([A-Za-z0-9_]+)/', $_GET['q'])) {
     header('Location: /');
     exit();
   }
 
   $category = htmlspecialchars($_GET['q']);
-  echo('link is ');
-  echo($category);
+  // echo('link is ');
+  // echo($category);
+
+  include('trending.html');
 
 ?>
